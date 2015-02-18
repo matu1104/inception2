9eb33d01da061303a7267bfa11d15862ffd9d601
Matías Cubero <matias.cubero@moove-it.com>
2015-02-18 09:34:06 -0200

    Initial commit
    Change-Id: Ieb28e6ab456566cd7da50accdbd40170a3dacc7d

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.19'
+
+gem 'mysql2'
+
+group :test do
+  gem 'test-unit'
+  gem 'shoulda'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterApp::Application.config.secret_token = '40ffc22ffff56c1e297b91cea992ec1e91dea54f5b324b1b2de329833cf179c0232285a749eae71c009c082d22e09e24f4411e87865c01a4bf2df5e851f72ffe'
